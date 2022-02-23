aa7548151384a6ffc727ff6ab8305e6015aa861c
Skinzin <SkinzinSTG_@hotmail.com>
2022-02-23 15:48:42 -0300

    First Project in Python

---
+++ generatePassword.py
@@ -0,0 +1,27 @@
+## Importações
+
+import random;
+import string;
+
+
+## Uma pergunta no Terminal.
+length = int(input("\nDigite o tamanho da senha:"))
+
+## Dados.
+
+toLower = string.ascii_lowercase
+toUpper = string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation
+
+## Juntando os dados.
+all = toLower + toUpper + numbers + symbols
+
+## Random
+temp = random.sample(all, length)
+
+## Criando a senha.
+password = "".join(temp)
+
+## Terminal.
+print(password)
